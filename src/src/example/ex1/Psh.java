39ecd52e53585ac7e61de385cc3bcf992d871726
Sungho95 <tjdgh1785@gmail.com>
2023-02-11 14:30:51 +0900

    Title: 폰켓몬, Time: 3.74 ms, Memory: 71.1 MB
    hash 사용

--- src/src/example/ex1/Psh.java
+++ src/src/example/ex1/Psh.java
@@ -1,9 +1,18 @@
 package src.example.ex1;
 
-public class Psh {
-    public String solution(int num) {
+import java.util.HashSet;
 
-        // 짝수와 홀수 판별 - num을 2로 나눈 나머지가 0이면 짝수, 아니면 홀수
-        return num % 2 == 0 ? "Even" : "Odd";
+public class Psh {
+    public int solution(int[] nums) {
+        // hashSet 생성
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        // hashSet에 포켓몬 담기(중복 제거됨)
+        for (int i = 0; i < nums.length; i++) {
+            hashSet.add(nums[i]);
+        }
+
+        // 둘 중 작은 것이 최대 종류
+        return Math.min(hashSet.size(), nums.length / 2);
     }
 }
