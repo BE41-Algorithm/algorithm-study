0111054073401d19791b72aa824626d98450801d
Lee JaeHyeok <dhfif718@gmail.com>
2023-02-22 18:30:57 +0900

    Title: 주식가격, Time: 26.58 ms, Memory: 67.6 MB

--- src/src/programmers/stackqueue6/JaeHyeok.java
+++ src/src/programmers/stackqueue6/JaeHyeok.java
@@ -18,19 +18,19 @@ public class JaeHyeok {
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < prices.length; i++) {
-            // 값이 떨어지는 주식의 시간을 저장
+            // 값이 떨어지는 주식의 시간(인덱스번호)을 저장
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                answer[stack.peek()] = i - stack.peek(); // 반환배열에 떨어지는 시간 초 저장
+                answer[stack.peek()] = i - stack.peek(); // 반환배열에 떨어지는 초 저장
                 // System.out.println("i - stack.peek() = " + (i-stack.peek()));
-                stack.pop(); // 값이 떨어진 초는 제거
+                stack.pop(); // 값이 떨어진 가격(인덱스번호)은 제거
             }
-            stack.push(i); // 값이 떨어지지않은 주식의 시간초를 저장
+            stack.push(i); // 순환시 가격의 인덱스번호를 저장
             // System.out.println("i = "+ i + " / stack = " + stack);
         }
 
         while (!stack.isEmpty()) {
-            // 값이 떨어지지않은 주식들, 마지막 인덱스부터 차례대로 빼오기
-            Integer last = stack.pop();
+            // 값이 떨어지지않은 주식들, 마지막 인덱스부터 차례대로 계산하기
+            int last = stack.pop();
             answer[last] = prices.length - last - 1;
         }
         return answer;
